9b85619759214b1c0c4356f45fabf91445e96bbe
Ritesh Rathod <rnrathod16@gmail.com>
2022-12-17 14:01:35 +0530

    Made Card Responsive

--- src/Components/Pricing/Pricing.jsx
+++ src/Components/Pricing/Pricing.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import Card from '../Card/Card'
 import Data from "../../JsonData/Data";
+import "./Pricing.css"
 
 const Pricing = () => {
     return (
         <>
             <div className="mt-4 m-auto" style={{ maxWidth: "85vw" }}>
                 <h1 className='text-center mb-5'>Pricing</h1>
-                <div className='d-flex justify-content-around'>
+                <div className='d-flex justify-content-around responsive'>
                     {Data.map((val) => {
                         return <Card val={val} />
                     })}
